94411f94fa3864f5927c76c37fc45ff2e8c939fc
tkdals5879 <tkdals58799@gmail.com>
2025-07-02 15:10:40 +0900

    description edit

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <h1>Visual Impact Design With Space</h1>
       <p className='description'>
         간단한 CSS만으로 <br />
-        시각적으로 인상적인 효과를 구현하기위해 노력합니다 <br />
+        시각적으로 인상적인 효과를 구현하였습니다 <br />
         모든 이미지소스는 Vecteezy에서 다운받았습니다
       </p>
     </div>
